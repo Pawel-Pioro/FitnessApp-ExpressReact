17d97773dca0457552d67d9311e0b3480e98e52a
Pawel-Pioro <pavvcp3650@gmail.com>
2024-06-19 09:15:53 +0100

    Initital commit

---
+++ backend/server.js
@@ -0,0 +1,42 @@
+require('dotenv').config()
+
+const express = require('express')
+const cors = require('cors')
+const mongoose = require('mongoose')
+const workoutRoutes = require('./routes/workouts')
+const userRoutes = require('./routes/user')
+
+// express app
+const app = express()
+
+// middleware
+app.use(express.json())
+app.use(cors())
+
+app.use((req, res, next) => {
+    console.log(req.path, req.method)
+    next()
+})
+
+// app.use(function (req, res, next) {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//     next();
+// });
+
+// routes
+app.use('/api/workouts', workoutRoutes)
+app.use('/api/user', userRoutes)
+
+// connect to db
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        // listen for requests
+        app.listen(process.env.PORT, () => {
+            console.log(`Server listening on port ${process.env.PORT}...`)
+        })
+    })
+    .catch((error) => {
+        console.log(error)
+    })
